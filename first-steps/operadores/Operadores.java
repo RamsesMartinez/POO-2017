91c0ff0b0e4ccba61007cc3cecd72bb0d0d11d8c
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-02-20 19:09:50 -0600

    Renamed folder examples. Added new classes

---
+++ first-steps/operadores/Operadores.java
@@ -0,0 +1,13 @@
+class Operadores {
+	public static void main(String[] args) {
+		int x = 6;
+		boolean y = (x>6) || ( x> 10);
+		System.out.println("hola: " + y);
+
+		boolean z = true;
+
+		if (z) {
+			System.out.println("EXITO!!");
+		}
+	}
+}
