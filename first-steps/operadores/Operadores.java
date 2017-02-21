77c096456387f984cb4388e4d673fa02da0971da
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-02-21 11:00:56 -0600

    Operator classes added

--- first-steps/operadores/Operadores.java
+++ first-steps/operadores/Operadores.java
@@ -9,5 +9,18 @@ class Operadores {
 		if (z) {
 			System.out.println("EXITO!!");
 		}
+
+		int j;
+		for (j = 0; j < 10 ; j++) {
+			System.out.println(j);
+		}
+
+		final String nombres[] =  new String[2];
+		nombres[0] = "Paquito";
+		nombres[1] = "Juanito";
+
+		for (String nombre : nombres) {
+			System.out.println(nombre);
+		}
 	}
 }
